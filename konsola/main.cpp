a9c414c221b1065d05e7fcd09ad854c8349d3649
brtke7 <brtke00@gmail.com>
2026-09-02 13:28:55 +0200

    feat(WashingMachine): created Washinemachine class, and implemented it's method

--- konsola/main.cpp
+++ konsola/main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 
-class Device {
+class Device 
+{
     public: 
-        void print(std::string text) {
+        void print(std::string text) 
+        {
             std::cout << text << std::endl; 
         }
 };
 
-int main() {
-    Device device;
+class WashingMachine : public Device
+{
+    private: 
+        int program_number = 0;
 
+    public: 
+        int set_program(int program_number) 
+        {
+            if (program_number >= 1 && program_number <= 12) 
+            {
+                this->program_number = program_number;
+            } else 
+            {
+                this->program_number = 0;
+            }
+
+            return this->program_number;
+        }
+};
+
+int main() {
     return 0;
 }
