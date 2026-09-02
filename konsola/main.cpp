c19e7c336744ab535174f404bc4476ad81d221b0
brtke7 <brtke00@gmail.com>
2026-09-02 13:14:57 +0200

    feat(main): created base class and it's methods

--- konsola/main.cpp
+++ konsola/main.cpp
@@ -1,1 +1,14 @@
-#include <stdio.h>
+#include <iostream>
+
+class Device {
+    public: 
+        void print(std::string text) {
+            std::cout << text << std::endl; 
+        }
+};
+
+int main() {
+    Device device;
+
+    return 0;
+}
