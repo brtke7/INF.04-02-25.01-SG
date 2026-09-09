cfc441b502929b39e20b75e8965f1a044aa366e9
brtke7 <brtke00@gmail.com>
2026-09-09 12:49:42 +0200

    feat(MainActivity.java): implemented vacuum cleaner toggling function, and improved set program number function by checking if input is not  empty

--- Mobilna/app/src/main/java/com/example/mobilna/MainActivity.java
+++ Mobilna/app/src/main/java/com/example/mobilna/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_submit;
     private EditText program_number;
     private TextView program_number_text;
+
+    private Button turn_on_vacuum_cleaner_btn;
+    private TextView is_turned_on;
+    private TextView vacuum_status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +33,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        btn_submit          = findViewById(R.id.button_washingmachine);
-        program_number      = findViewById(R.id.edit_text_washingmaschine);
-        program_number_text = findViewById(R.id.program_number);
+        btn_submit            = findViewById(R.id.button_washingmachine);
+        program_number        = findViewById(R.id.edit_text_washingmaschine);
+        program_number_text   = findViewById(R.id.program_number);
+        turn_on_vacuum_cleaner_btn = findViewById(R.id.vacuum_cleaner_button);
+        is_turned_on          = findViewById(R.id.is_vacuum_cleaner_turned_on);
+        vacuum_status         = findViewById(R.id.vacuum_cleaner_status);
+
 
         btn_submit.setOnClickListener(v -> set_program_number());
+        turn_on_vacuum_cleaner_btn.setOnClickListener(v -> toggle_vacuum_cleaner());
     }
 
     public void set_program_number() {
-        int program_value = Integer.parseInt(this.program_number.getText().toString());
+        if (this.program_number.getText().length() != 0) {
+            int program_value = Integer.parseInt(this.program_number.getText().toString());
 
-        if ( program_value >= 1 && program_value <= 12 )
-        {
-            program_number_text.setText("Numer prania: " + program_value);
+            if (program_value >= 1 && program_value <= 12) {
+                program_number_text.setText("Numer prania: " + program_value);
+            }
+        }
+    }
+
+    public void toggle_vacuum_cleaner()
+    {
+        String status = turn_on_vacuum_cleaner_btn.getText().toString();
+
+        if (status.equalsIgnoreCase("WŁĄCZ")) {
+            turn_on_vacuum_cleaner_btn.setText("WYŁĄCZ");
+            is_turned_on.setText("Odkurzacz: Włączony");
+        } else {
+            turn_on_vacuum_cleaner_btn.setText("WŁĄCZ");
+            is_turned_on.setText("Odkurzacz: Wyłączony");
         }
     }
 }
