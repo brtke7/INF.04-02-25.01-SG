1b292393fccd425c1dde7e9913a9929abd38855c
brtke7 <brtke00@gmail.com>
2026-09-09 12:37:56 +0200

    feat(MainActivity.java): implemented set_program_number function

--- Mobilna/app/src/main/java/com/example/mobilna/MainActivity.java
+++ Mobilna/app/src/main/java/com/example/mobilna/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mobilna;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_submit;
+    private EditText program_number;
+    private TextView program_number_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn_submit          = findViewById(R.id.button_washingmachine);
+        program_number      = findViewById(R.id.edit_text_washingmaschine);
+        program_number_text = findViewById(R.id.program_number);
+
+        btn_submit.setOnClickListener(v -> set_program_number());
+    }
+
+    public void set_program_number() {
+        int program_value = Integer.parseInt(this.program_number.getText().toString());
+
+        if ( program_value >= 1 && program_value <= 12 )
+        {
+            program_number_text.setText("Numer prania: " + program_value);
+        }
     }
 }
